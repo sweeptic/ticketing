9c7099aaceed9ba43e5a38daa2d581657ed5fe72
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-15 12:38:52 +0100

    131. moving logic into errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,6 +1,7 @@
 import { ValidationError } from 'express-validator';
 
 export class DatabaseConnectionError extends Error {
+  statusCode = 500;
   reason = 'Error connecting to database';
   constructor() {
     super();
@@ -8,5 +9,9 @@ export class DatabaseConnectionError extends Error {
     //  https://stackoverflow.com/questions/41102060/typescript-extending-error-class
     // This is because when you call Error as a function (rather than via new or, in ES2015, super or Reflect.construct), it ignores this and creates a new Error.
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,6 +1,7 @@
 import { ValidationError } from 'express-validator';
 
 export class RequestValidationError extends Error {
+  statusCode = 400;
   // errors: ValidationError[]
   constructor(public errors: ValidationError[]) {
     //  this.errors = errors;
@@ -9,5 +10,11 @@ export class RequestValidationError extends Error {
     //  https://stackoverflow.com/questions/41102060/typescript-extending-error-class
     // This is because when you call Error as a function (rather than via new or, in ES2015, super or Reflect.construct), it ignores this and creates a new Error.
     Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(error => {
+      return { message: error.msg, field: error.param };
+    });
   }
 }
