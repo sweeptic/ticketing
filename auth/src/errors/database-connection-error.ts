a7dce336509e1226e2cf6f1332a27243b68ae1c0
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-15 13:32:51 +0100

    132. verifiyng our custom error

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,10 @@
-import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class DatabaseConnectionError extends Error {
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = 'Error connecting to database';
   constructor() {
-    super();
+    super('Error connecting to db');
 
     //  https://stackoverflow.com/questions/41102060/typescript-extending-error-class
     // This is because when you call Error as a function (rather than via new or, in ES2015, super or Reflect.construct), it ignores this and creates a new Error.
