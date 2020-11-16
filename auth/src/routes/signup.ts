2852d111806eafb3bacb8ffc334477f517f274ef
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-16 16:53:57 +0100

    145. user creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../../models/user';
 
 const router = express.Router();
 
@@ -14,19 +14,26 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be in 4 and 20 characters.'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
 
-    // const { email, password } = req.body;
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    console.log('Creating user...');
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+
+    await user.save();
+
+    return res.status(201).send(user);
   }
 );
 
