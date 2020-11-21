a08e1a5d835e5aab1e9bd0c36b3f191df3090125
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-21 13:33:32 +0100

    formatting JSON properties

--- auth/models/user.ts
+++ auth/models/user.ts
@@ -19,16 +19,28 @@ interface UserDoc extends mongoose.Document {
 }
 
 //describe properties
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 // So this is a middleware function implemented in mongoose.
 // Anytime we attempt to save a document to our database we are going to execute this function right here
