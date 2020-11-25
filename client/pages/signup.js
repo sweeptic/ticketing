7b2c7aa001178353de5ada9137073a16afac7e54
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-25 14:40:05 +0100

    204. handling email and password inputs

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
-    <form className='container'>
+    <form className='container' onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className='form-group'>
         <label>Email Address</label>
-        <input className='form-control' />
+        <input
+          className='form-control'
+          value={email}
+          onChange={ev => setEmail(ev.target.value)}
+        />
       </div>
       <div className='form-group'>
         <label>Password</label>
-        <input type='password' className='form-control' />
+        <input
+          type='password'
+          className='form-control'
+          value={password}
+          onChange={ev => setPassword(ev.target.value)}
+        />
       </div>
       <button className='btn btn-primary'>Sign Up</button>
     </form>
