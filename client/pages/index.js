f4ac7970ab7da4784122457677c713da5c4bea46
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-26 16:07:46 +0100

    219. passing through the cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,25 +1,19 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  // 'http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser',
   console.log(currentUser);
 
   return <h1>landing page!</h1>;
 };
 
-//http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser
-LandingPage.getInitialProps = async () => {
-  //   const response = await axios.get('/api/users/currentuser');
-
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     //  we are on server
     //request to ingress...
     const { data } = await axios.get(
       'http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser',
       {
-        headers: {
-          Host: 'ticketing.dev',
-        },
+        headers: req.headers,
       }
     );
     return data;
