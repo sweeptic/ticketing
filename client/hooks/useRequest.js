67087c04b5b8798b0af97aca987d87172d5a0aa4
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-26 13:34:50 +0100

    209 an onSuccess callback

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -1,21 +1,28 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
       setErrors(null);
       const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
       return response.data;
     } catch (err) {
       setErrors(
         <div className='alert alert-danger'>
           <h4>Oooop....</h4>
-          {err.response.data.errors.map((error, index) => (
-            <li key={index}> {error.message}</li>
-          ))}
+          <ul className='my-0'>
+            {err.response.data.errors.map((error, index) => (
+              <li key={index}> {error.message}</li>
+            ))}
+          </ul>
         </div>
       );
     }
--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import useRequest from '../hooks/useRequest';
+import Router from 'next/router';
 
 const signup = () => {
   const [email, setEmail] = useState('');
@@ -11,11 +12,13 @@ const signup = () => {
       email,
       password,
     },
+    onSuccess: () => Router.push('/'),
   });
 
   const onSubmit = async event => {
     event.preventDefault();
-    doRequest();
+
+    await doRequest();
   };
 
   return (
