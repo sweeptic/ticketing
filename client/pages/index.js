a352232b62be8b97372fe62662e347f5fc2d2034
Attila <59293104+sweeptic@users.noreply.github.com>
2020-11-27 16:53:11 +0100

    221. content on the landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,11 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-
-  return <h1>landing page!</h1>;
+  return currentUser ? (
+    <h1>You are sign in</h1>
+  ) : (
+    <h1>You are not signed id</h1>
+  );
 };
 
 LandingPage.getInitialProps = async context => {
