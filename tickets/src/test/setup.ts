d481fe6bc32518d397e4cdfa06d977ae056e8423
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-03 12:23:40 +0100

    251 faking authentication during tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -42,18 +42,10 @@ afterAll(async () => {
 });
 
 global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
-
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
-
-  const cookie = response.get('Set-Cookie');
-
-  return cookie;
+  //build a JWT payload. (id, email)
+  //create the JWT!
+  //build a session object {jwt: MY_JWT}
+  //Turn that session into JSON
+  //Take JSON and encode it as base64
+  //return a string thats the cookie with the encoded data
 };
