6aa579bd6c5aa25733352bdaf2e5c43e7d6c8cea
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-03 13:06:53 +0100

    254. validating title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,24 @@
-import { requireAuth } from '@sgtickets-sweeptic/common';
+import { requireAuth, validateRequest } from '@sgtickets-sweeptic/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+
+  [body('title').not().isEmpty().withMessage('Title is required')],
+  [
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+
+  validateRequest,
+
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
