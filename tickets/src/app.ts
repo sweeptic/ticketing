45f941e2d50481b9457d4ed854a8a42ece2ea337
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-03 11:58:47 +0100

    250. adding auth protection

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,11 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@sgtickets-sweeptic/common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@sgtickets-sweeptic/common';
 import { createTicketRouter } from './routes/new';
 
 const app = express();
@@ -15,6 +19,7 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
 app.use(createTicketRouter);
 
