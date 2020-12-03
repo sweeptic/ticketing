2cf482d6996aacd056a3869aa36b022632c0d002
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-03 15:23:47 +0100

    261 better error logging

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,8 +1,11 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 
 it('returns a 404 if the ticket is not found', async () => {
-  await request(app).post('/api/tickets/fdgfdgfdgfdg').send();
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app).post(`/api/tickets/${id}`).send();
   expect(404);
 });
 
