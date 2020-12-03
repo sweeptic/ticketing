bb7193a5b472d8359809e97079c7a5a1147577e3
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-03 12:37:30 +0100

    building a session

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -8,7 +8,11 @@ it('has a route handler listening to /api/tickets for post requests', async () =
 });
 
 it('can only be accessed if the user is signed in', async () => {
-  request(app).post('/api/tickets').send({}).expect(401);
+  request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({})
+    .expect(401);
 });
 
 it('returns a status other than 401 if the user is signed in', async () => {
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,12 +2,14 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
+
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -41,11 +43,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
+global.signin = () => {
   //build a JWT payload. (id, email)
+  const payload = {
+    id: 'dfsdfgrere',
+    email: 'test@test.com',
+  };
+
   //create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
   //build a session object {jwt: MY_JWT}
+  const session = { jwt: token };
+
   //Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
   //Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
   //return a string thats the cookie with the encoded data
+  return [`express:sess=${base64}`];
 };
