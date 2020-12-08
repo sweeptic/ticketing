607c40f7bfe5271044249b4e7ece3533042bf17f
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-08 16:55:51 +0100

    292. extending the listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,6 +1,5 @@
 import { randomBytes } from 'crypto';
 import nats, { Message, Stan } from 'node-nats-streaming';
-import { runInThisContext } from 'vm';
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
@@ -14,28 +13,7 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-services');
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(
-        `Received event #${msg.getSequence()}, with data: ${JSON.parse(data)}`
-      );
-    }
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -79,5 +57,16 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data);
+
+    msg.ack();
   }
 }
