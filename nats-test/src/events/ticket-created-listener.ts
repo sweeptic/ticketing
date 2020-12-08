981c4913940d87000015b14786582c161ff056fa
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-08 17:36:06 +0100

    297. enforcing listener subjects

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,9 +1,10 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './ticket-created-events';
 import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
   onMessage(data: any, msg: Message) {
