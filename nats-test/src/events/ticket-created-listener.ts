81dda5419637d2ac12a5f2a07f05e6f108f28f42
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-09 17:36:52 +0100

    updating the common module

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,4 +1,4 @@
-import { Listener } from './base-listener';
+import { Listener } from '../../../common/src/events/base-listener';
 import { Message } from 'node-nats-streaming';
 import { TicketCreatedEvent } from './ticket-created-events';
 import { Subjects } from './subjects';
