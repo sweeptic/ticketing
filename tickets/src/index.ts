04e855d849afb2848ac33e99083df12bf2c4ef08
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-10 13:51:49 +0100

    311. singleton implementation

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -12,6 +12,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'dfsfeew', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,5 +1,21 @@
 import nats, { Stan } from 'node-nats-streaming';
 
-class NatsWrapper {}
+class NatsWrapper {
+  private _client?: Stan; //? - tell ts this value may be undefined som period of time
 
-export const natsWrapper = new NatsWrapper();
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise((resolve, reject) => {
+      this._client!.on('connect', () => {
+        console.log('Connected to NATS');
+        resolve();
+      });
+      this._client!.on('error', err => {
+        reject();
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper(); //exporting instance, not class ! thats why called singleton
