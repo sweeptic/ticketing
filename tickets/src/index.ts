39631bca972063441a314c4557490ea3429e9dae
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-11 13:29:56 +0100

    323. NATS env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,8 +11,24 @@ const start = async () => {
     throw new Error('MONGO_URI must be defined');
   }
 
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  if (!process.env.CLUSTER_ID) {
+    throw new Error('CLUSTER_ID must be defined');
+  }
+
   try {
-    await natsWrapper.connect('ticketing', 'dfsfeew', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+      process.env.CLUSTER_ID
+    );
 
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
