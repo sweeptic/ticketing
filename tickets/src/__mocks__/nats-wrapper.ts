ebd8ffd2d84e4b58f91029f1e9b8c0e9bdba811a
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-11 13:19:47 +0100

    322. ensuring mock invocations

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,7 +1,15 @@
 export const natsWrapper = {
   client: {
-    publish: (subject: string, data: string, callback: () => void) => {
-      callback();
-    },
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
+
+    // publish: (subject: string, data: string, callback: () => void) => {
+    //   callback();
+    // },
   },
 };
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -82,4 +83,21 @@ it('creates a ticket with valid inputs', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(20);
   expect(tickets[0].title).toEqual(20);
+});
+
+it('publishes an event', async () => {
+  const title = 'wefwef';
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: title,
+      price: 20,
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+
+  // console.log(natsWrapper);
 });
