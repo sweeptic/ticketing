e7e91ab895e1f76668dce6d2fade2baad9fe6cdb
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-13 17:46:27 +0100

    337. finding reserved ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import {
+  BadRequestError,
   NotFoundError,
+  OrderStatus,
   requireAuth,
   validateRequest,
 } from '@sgtickets-sweeptic/common';
@@ -30,8 +32,23 @@ router.post(
     if (!ticket) {
       throw new NotFoundError();
     }
-
-    //make sure that this ticket is not alredy reserved
+    // Make sure that this ticket is not already reserved
+    // Run query to look at all orders.  Find an order where the ticket
+    // is the ticket we just found *and* the orders status is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
 
     //calculate an expiration date for this order
 
