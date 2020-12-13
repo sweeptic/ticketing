50bda08174c624bd83897dbcc8a86296d98379ac
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-13 19:04:00 +0100

    344. fething a users orders

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,12 +1,14 @@
+import { requireAuth } from '@sgtickets-sweeptic/common';
 import express, { Request, Response } from 'express';
-// import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders', async (req: Request, res: Response) => {
-  //   const tickets = await Ticket.find({});
-
-  res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate('ticket');
+  res.send(orders);
 });
 
 export { router as indexOrderRouter };
