9b0b39efcb50df0693de100b813944f8aad90ade
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-14 12:04:01 +0100

    345. a slightly complicated test

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,63 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
+it('fetches orders for an particular user', async () => {
+  // create three tickets
+  const ticket1 = await buildTicket();
+  const ticket2 = await buildTicket();
+  const ticket3 = await buildTicket();
+
+  //  await request(app)
+  //    .post('/api/orders')
+  //    .set('Cookie', global.signin())
+  //    .send({ ticketId: ticket.id })
+  //    .expect(201);
+
+  // create on order as user #1
+  const user1 = global.signin();
+  const user2 = global.signin();
+
+  // create one order as user #1
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({ ticketId: ticket1.id })
+    .expect(201);
+
+  // create two orders as user #2
+  const { body: orderOne } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({ ticketId: ticket2.id })
+    .expect(201);
+  const { body: orderTwo } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({ ticketId: ticket3.id })
+    .expect(201);
+
+  //make request to get orders for User #2
+  const response = await request(app)
+    .get('/api/orders')
+    .set('Cookie', user2)
+    .expect(200);
+
+  //make sure we only got the orders for user #2
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[0].ticket.id).toEqual(ticket2.id);
+  expect(response.body[1].ticket.id).toEqual(ticket3.id);
+});
