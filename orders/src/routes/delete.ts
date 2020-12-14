2cc66d8ce85a4136b05344a76aec94d09b8b9b33
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-14 12:37:12 +0100

    348. cancelling an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,71 +1,33 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@sgtickets-sweeptic/common';
 import express, { Request, Response } from 'express';
-// import { Ticket } from '../models/ticket';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  //   const tickets = await Ticket.find({});
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
 
-  res.send({});
-});
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
-
-// import express, { Request, Response } from 'express';
-// import { body } from 'express-validator';
-// import {
-//   NotAuthorizedError,
-//   validateRequest,
-//   NotFoundError,
-//   requireAuth,
-// } from '@sgtickets-sweeptic/common';
-// import { Ticket } from '../models/ticket';
-// import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
-// import { natsWrapper } from '../nats-wrapper';
-
-// const router = express.Router();
-
-// router.put(
-//   '/api/tickets/:id',
-//   requireAuth,
-
-//   [
-//     body('title').not().isEmpty().withMessage('Title is required'),
-
-//     body('price')
-//       .isFloat({ gt: 0 })
-//       .withMessage('Price must be provided and must be greater than 0 '),
-//   ],
-
-//   validateRequest,
-
-//   async (req: Request, res: Response) => {
-//     const ticket = await Ticket.findById(req.params.id);
-
-//     if (!ticket) {
-//       throw new NotFoundError();
-//     }
-
-//     if (ticket.userId !== req.currentUser!.id) {
-//       throw new NotAuthorizedError();
-//     }
-
-//     ticket.set({
-//       title: req.body.title,
-//       price: req.body.price,
-//     });
-
-//     await ticket.save();
-
-//     new TicketUpdatedPublisher(natsWrapper.client).publish({
-//       id: ticket.id,
-//       title: ticket.title,
-//       price: ticket.price,
-//       userId: ticket.userId,
-//     });
-
-//     res.send(ticket);
-//   }
-// );
-
-// export { router as updateTicketRouter };
