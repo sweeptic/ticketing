ef85efed6aac61c107758c93c089138b6d65eeb1
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-15 11:33:46 +0100

    358. blueprint for listener typo

--- orders/src/listeners/ticket-created-listener.ts
+++ orders/src/listeners/ticket-created-listener.ts
@@ -5,3 +5,11 @@ import {
   Listener,
   Subjects,
 } from '@sgtickets-sweeptic/common';
+import { Ticket } from '../models/ticket';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = 'orders-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+}
