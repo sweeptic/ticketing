1273d163fb847ea077bc8a0e7dfb430047a3fc7d
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-15 11:41:39 +0100

    359. a few more reminders

--- orders/src/listeners/ticket-created-listener.ts
+++ orders/src/listeners/ticket-created-listener.ts
@@ -1,15 +1,15 @@
 import { Message } from 'node-nats-streaming';
-
 import {
   TicketCreatedEvent,
   Listener,
   Subjects,
 } from '@sgtickets-sweeptic/common';
 import { Ticket } from '../models/ticket';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
