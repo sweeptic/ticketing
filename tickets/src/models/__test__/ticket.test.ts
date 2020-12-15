7eca338582e98587eafc4e920ad9d1dd8c34626e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-15 13:55:52 +0100

    371 one more test

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -33,3 +33,20 @@ it('implements optimistic concurrency control', async done => {
   throw new Error('Should not reach this point');
   //   expect(() => {}).toThrow();
 });
+
+it('increment the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
