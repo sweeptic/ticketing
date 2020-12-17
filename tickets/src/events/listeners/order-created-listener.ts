fff184d7cf39b84e5608bf6fd13fc2d519f69863
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-17 13:19:15 +0100

    390 reserving a ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,28 @@ import {
 } from '@sgtickets-sweeptic/common';
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // find the ticket that the order is reserved
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    //if no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    //mark te ticket as being reserved by setting its orderId property
+    ticket.set({ orderId: data.id });
+
+    //save the ticket
+    await ticket.save();
+
+    //ack the message
+    msg.ack();
+  }
 }
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -15,6 +15,7 @@ interface TicketDoc extends mongoose.Document {
   price: number;
   userId: string;
   version: number;
+  orderId?: string;
 }
 
 // ticket model interface
@@ -36,6 +37,10 @@ const ticketSchema = new mongoose.Schema(
     userId: {
       type: String,
       required: true,
+    },
+    orderId: {
+      type: String,
+      required: false,
     },
   },
   {
