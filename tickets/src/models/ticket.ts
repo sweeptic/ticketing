1d328abd97ebfc4ae5216f18da1a2d4a0e94654a
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-17 15:33:21 +0100

    400. rejecting edits of reserved tickets

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,33 +1,28 @@
 import mongoose from 'mongoose';
 import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
-// An new ticket interface - TO TYPESCRIPT
 interface TicketAttrs {
   title: string;
   price: number;
   userId: string;
 }
 
-// ticket document interface
-//to sadd some additional properties in the future
 interface TicketDoc extends mongoose.Document {
-  title: string; // typescript type
+  title: string;
   price: number;
   userId: string;
   version: number;
   orderId?: string;
 }
 
-// ticket model interface
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
 
-//describe properties - MONGOOSE VALUE TYPE
 const ticketSchema = new mongoose.Schema(
   {
     title: {
-      type: String, //JS global string constructor
+      type: String,
       required: true,
     },
     price: {
@@ -40,13 +35,11 @@ const ticketSchema = new mongoose.Schema(
     },
     orderId: {
       type: String,
-      required: false,
     },
   },
   {
     toJSON: {
       transform(doc, ret) {
-        //modify data
         ret.id = ret._id;
         delete ret._id;
       },
@@ -56,9 +49,8 @@ const ticketSchema = new mongoose.Schema(
 ticketSchema.set('versionKey', 'version');
 ticketSchema.plugin(updateIfCurrentPlugin);
 
-//with the help of TS
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket({ attrs });
+  return new Ticket(attrs);
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,6 +5,7 @@ import {
   validateRequest,
   NotFoundError,
   requireAuth,
+  BadRequestError,
 } from '@sgtickets-sweeptic/common';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
@@ -31,6 +32,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError('Cannot edit a reserved ticket');
     }
 
     if (ticket.userId !== req.currentUser!.id) {
