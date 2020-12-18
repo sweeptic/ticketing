74d569dbe7f46bc3e32ec6489681016704c85d01
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-18 15:44:25 +0100

    412 defining the expiration complete event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,4 +1,8 @@
-import { Listener, OrderCreatedEvent, Subjects } from '@sgtickets/common';
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+} from '@sgtickets-sweeptic/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { expirationQueue } from '../../queues/expiration-queue';
@@ -8,9 +12,17 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('waiting this many milliseconds to process the job:', delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: 10000,
+      }
+    );
 
     msg.ack();
   }
