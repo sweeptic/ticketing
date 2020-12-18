e9ccb528270ead9916a2220c5f5f32b159e482a7
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-18 15:19:11 +0100

    410 testing job processing

--- expiration/src/queues/expiration-queue.ts
+++ expiration/src/queues/expiration-queue.ts
@@ -4,12 +4,15 @@ interface Payload {
   orderId: string;
 }
 
+//define queue
 const expirationQueue = new Queue<Payload>('order:expiration', {
   redis: {
     host: process.env.REDIS_HOST,
   },
 });
 
+//receive job
+//code to process a job
 expirationQueue.process(async job => {
   console.log(
     'I want to publish an expiration:complete event for orderId',
