451538c757a6e0edfa9e25205b47cb6a1e0f3b19
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-18 14:42:58 +0100

    406. listener creation

--- expiration/src/nats-wrapper.ts
+++ expiration/src/nats-wrapper.ts
@@ -1,9 +1,8 @@
 import nats, { Stan } from 'node-nats-streaming';
 
 class NatsWrapper {
-  private _client?: Stan; //? - tell ts this value may be undefined some period of time
+  private _client?: Stan;
 
-  //typescript getter
   get client() {
     if (!this._client) {
       throw new Error('Cannot access NATS client before connecting');
@@ -15,13 +14,13 @@ class NatsWrapper {
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
 
-    return new Promise((resolve, reject) => {
-      this.client!.on('connect', () => {
+    return new Promise<void>((resolve, reject) => {
+      this.client.on('connect', () => {
         console.log('Connected to NATS');
         resolve();
       });
-      this.client!.on('error', err => {
-        reject();
+      this.client.on('error', err => {
+        reject(err);
       });
     });
   }
